ed0c264bd73a9c61936bc3e920eb949a46e01295
Lucas Pina <ti.lucaspina@gmail.com>
2026-02-02 19:30:55 -0300

    continuar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import s from "./header.module.scss";
 import { Link } from "react-router-dom";
 import logo from "../../assets/icones/logo.png";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    function menuMobile() {
+      setIsMobile(window.innerWidth <= 768);
+
+      if (window.innerWidth > 768) setMenuAberto(false);
+    }
+    window.addEventListener("resize", menuMobile);
+    return () => window.removeEventListener("resize", menuMobile);
+  }, []);
+
   return (
     <>
       <header className={s.header}>
@@ -13,33 +28,53 @@ const Header = () => {
             alt="Imagem de logo do projeto connect sendo representada por uma mão segurando um coração, simbolizando apoio"
           />
         </Link>
-
-        <nav className={s.nav}>
-          <Link className={s.Link} to={"/doacao"}>
-            Doação
-          </Link>
-          <Link className={s.Link} to={"/voluntariado"}>
-            Voluntariado
-          </Link>
-          <Link className={s.Link} to={"/mentoria"}>
-            Mentoria
-          </Link>
-          <Link className={s.Link} to={"/eventosEP"}>
-            Eventos
-          </Link>
-        </nav>
+        {!isMobile && (
+          <nav className={s.nav}>
+            <Link className={s.Link} to={"/doacao"}>
+              Doação
+            </Link>
+            <Link className={s.Link} to={"/voluntariado"}>
+              Voluntariado
+            </Link>
+            <Link className={s.Link} to={"/mentoria"}>
+              Mentoria
+            </Link>
+            <Link className={s.Link} to={"/eventosEP"}>
+              Eventos
+            </Link>
+          </nav>
+        )}
 
         <img
           className={s.imgUser}
           src="https://avatars.githubusercontent.com/u/86132063?v=4"
           alt="Imagem do usuário"
+          onClick={() => setMenuAberto(!menuAberto)}
         />
       </header>
-      <nav className={s.navUser}>
-        <Link>Lucas Pina</Link>
-        <Link>Meu voluntariado</Link>
-        <Link>Configurações da conta</Link>
-        <Link>Sair</Link>
+      <nav className={menuAberto ? s.navUser : s.closedNavUser}>
+        <Link to={"/usuario"} onClick={() => setMenuAberto(false)}>
+          Lucas Pina
+        </Link>
+        <Link onClick={() => setMenuAberto(false)}>Meu voluntariado</Link>
+        <Link onClick={() => setMenuAberto(false)}>Configurações da conta</Link>
+        {isMobile && (
+          <div>
+            <Link to={"/doacao"} onClick={() => setMenuAberto(false)}>
+              Doação
+            </Link>
+            <Link to={"/voluntariado"} onClick={() => setMenuAberto(false)}>
+              Voluntariado
+            </Link>
+            <Link to={"/mentoria"} onClick={() => setMenuAberto(false)}>
+              Mentoria
+            </Link>
+            <Link to={"/eventosEp"} onClick={() => setMenuAberto(false)}>
+              Eventos
+            </Link>
+          </div>
+        )}
+        <Link onClick={() => setMenuAberto(false)}>Sair</Link>
       </nav>
     </>
   );
