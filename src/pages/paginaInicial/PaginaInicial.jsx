1b8f3853acad74ba716c3a020437b08577ad8d9b
Lucas Pina <ti.lucaspina@gmail.com>
2026-02-04 19:32:56 -0300

    iniciando pagina inicial

--- src/pages/paginaInicial/PaginaInicial.jsx
+++ src/pages/paginaInicial/PaginaInicial.jsx
@@ -1,11 +1,37 @@
-
+import s from "./paginaInicial.module.scss";
+import banner from "../../assets/img/banner.png";
 
 const PaginaInicial = () => {
   return (
-    <main>
-      <h1>Página Inicial</h1>
+    <main className={s.main}>
+      <section className={s.info}>
+        <h1>Projetos Sociais que transformam</h1>
+        <p>
+          Conectamos sua empresa com projetos sociais impactantes. Juntos,
+          criamos mudanças reais na comunidade.
+        </p>
+        <button>Cadastrar Empresa ➔</button>
+        <section className={s.infoDados}>
+          <article>
+            <h2>500+</h2>
+            <h3>Empresas Voluntárias</h3>
+          </article>
+          <article>
+            <h2>1.2K+</h2>
+            <h3>Realizados</h3>
+          </article>
+          <article>
+            <h2>50K+</h2>
+            <h3>Impactadas</h3>
+          </article>
+        </section>
+      </section>
+      <img
+        src={banner}
+        alt="imagem de duas mãos juntas com varios bonequinhos recortados de papel formando uma ciranda com um globo ao meio, representando responsabilidade social e ambiental global"
+      />
     </main>
-  )
-}
+  );
+};
 
-export default PaginaInicial
+export default PaginaInicial;
