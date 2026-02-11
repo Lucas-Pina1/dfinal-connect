f2a4dde61285b321eeb026c2023ad8481e1dbc61
Lucas Pina <ti.lucaspina@gmail.com>
2026-02-11 09:44:30 -0300

    finalizado

--- src/pages/eventosEP/EventosEP.jsx
+++ src/pages/eventosEP/EventosEP.jsx
@@ -1,11 +1,49 @@
+import s from "../Style.module.scss";
 
+import InfoCard from "../../components/infoCard/infoCard";
+// imagens
+import jovens from "../../assets/img/jovens.png";
+import tecTransforma from "../../assets/img/tecTransforma.png";
+import primeiroEmprego from "../../assets/img/primeiroEmprego.png";
 
 const EventosEP = () => {
   return (
-    <main>
-      <h1>Página de Eventos</h1>
+    <main className={s.main}>
+      <h1>Eventos & Palestras</h1>
+      <section>
+        <article>
+          <InfoCard
+            img={jovens}
+            alt="varios jovens participando de uma roda de conversa"
+            subtitulo="Empoderando Jovens para o Futuro"
+            paragrafo="Atividade: Palestra motivacional sobre liderança jovem e transformação social."
+            segParagrafo = "Impacto: Inspirar adolescentes a se tornarem agentes de mudança em suas comunidades"
+            textoBotao="Quero participar"
+          />
+        </article>
+        <article>
+          <InfoCard
+            img={tecTransforma}
+            alt="Varias pessoas tendo aulas em um computador"
+            subtitulo="Tecnologia que Transforma"
+            paragrafo="Atividade: Workshop de introdução à programação e inovação digital."
+            segParagrafo = "Impacto: Preparar jovens para o mercado de trabalho através da tecnologia"
+            textoBotao="Quero participar"
+          />
+        </article>
+        <article>
+          <InfoCard
+            img={primeiroEmprego}
+            alt="Duas pessoas coversando em frente á um computador"
+            subtitulo="Carreira e Primeiro Emprego"
+            paragrafo="Atividade: Palestra com profissionais de RH sobre como preparar currículo, entrevistas e postura no mercado"
+            segParagrafo = "Impacto: Ajudar jovens a conquistar oportunidades de trabalho."
+            textoBotao="Quero participar"
+          />
+        </article>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default EventosEP
+export default EventosEP;
--- src/pages/mentoria/Mentoria.jsx
+++ src/pages/mentoria/Mentoria.jsx
@@ -1,11 +1,46 @@
+import s from "../Style.module.scss";
 
+import InfoCard from "../../components/infoCard/infoCard";
+// imagens
+import carreira from "../../assets/img/carreira.png";
+import experiencia from "../../assets/img/experiencia.png";
+import acompanhamento from "../../assets/img/acompanhamento.png";
 
 const Mentoria = () => {
   return (
-    <main>
-      <h1>Página de Mentoria</h1>
+    <main className={s.main}>
+      <h1>Mentoria</h1>
+      <section>
+        <article>
+          <InfoCard
+            img={carreira}
+            alt="Pessoas coversando em uma mesa e alinhando opniões"
+            subtitulo="Mentoria de Carreira e Emprego"
+            paragrafo="Orientação sobre currículo, entrevistas e primeiros passos no mercado de trabalho."
+            textoBotao="Quero participar"
+          />
+        </article>
+        <article>
+          <InfoCard
+            img={experiencia}
+            alt="Pessoas coversando em uma mesa, compartilhando conhecimento "
+            subtitulo="Compartilhe Experiência"
+            paragrafo="Oriente jovens e profissionais iniciantes em sua área."
+            textoBotao="Quero participar"
+          />
+        </article>
+        <article>
+          <InfoCard
+            img={acompanhamento}
+            alt="Duas pessoas coversando em frente á um computador"
+            subtitulo="Acompanhamento"
+            paragrafo="Participe como guia em jornadas de aprendizado e desenvolvimento."
+            textoBotao="Quero participar"
+          />
+        </article>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Mentoria
+export default Mentoria;
